5086ddb1541b007692fedd8b9bb7a4ada499904a
Omer Akram <om26er@gmail.com>
2017-09-04 00:37:03 +0500

    Minor changes; use devmode for now

--- wamp/display_component.py
+++ wamp/display_component.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import os
 
 from autobahn.wamp.types import RegisterOptions
 from autobahn.asyncio.wamp import (
@@ -16,7 +17,7 @@ LOGGER.setLevel(logging.INFO)
 class DisplayController(ApplicationSession):
     async def onJoin(self, details):
         LOGGER.info('Joined realm \'{}\'.'.format(details.realm))
-        display = manager.Display('kde')
+        display = manager.Display(os.environ.get('XDG_CURRENT_DESKTOP', 'KDE').lower())
         options = RegisterOptions(match='exact', invoke='roundrobin')
         await self.register(display.is_locked, 'com.om26er.ldm.is_screen_locked', options)
         await self.register(display.lock, 'com.om26er.ldm.lock_screen', options)
@@ -28,7 +29,7 @@ class DisplayController(ApplicationSession):
 
 
 def main():
-    runner = ApplicationRunner(url=u'ws://www.om26er.com/ws', realm=u'ldm')
+    runner = ApplicationRunner(url=u'ws://ldm.om26er.com/ws', realm=u'ldm')
     runner.run(DisplayController)
 
 
